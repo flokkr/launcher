155176eeb9853d34f5874b963b737721f555a5f8
Elek, Márton <elek@users.noreply.github.com>
2017-11-12 21:05:49 +0100

    fix installer script to support non root environments

--- plugins/005_installer/installer.sh
+++ plugins/005_installer/installer.sh
@@ -2,19 +2,23 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for INSTALLER in $(env | grep INSTALLER_); do
+		  ID=$(id -u -n)
 		  KEY=$(echo $INSTALLER | awk -F '=' '{print $1}')
 		  COMPONENT=$(echo $KEY | awk -F '_' '{print tolower($2)}')
 		  URL=$(echo $INSTALLER | awk -F '=' '{print $2}')
-		  rm -rf /opt/$COMPONENT
-		  rm -rf /opt/unpack
-		  mkdir -p /opt/unpack
-		  mkdir -p /opt/download
+		  sudo rm -rf /opt/$COMPONENT
+		  sudo rm -rf /opt/unpack
+		  sudo mkdir -p /opt/unpack
+		  sudo mkdir -p /opt/download
+		  sudo chown $ID /opt/unpack
+		  sudo chown $ID /opt/download
 		  DESTFILE=/opt/download/$COMPONENT.tar.gz
 		  if [ ! -f "$DESTFILE" ]; then
 		     wget $URL -O $DESTFILE
 	          fi
 		  tar xzf $DESTFILE -C /opt/unpack
-                  mv /opt/unpack/* /opt/$COMPONENT
+                  sudo mv /opt/unpack/* /opt/$COMPONENT
+                  sudo chown $ID /opt/$COMPONENT
                   rm -rf /opt/unpack
 done
 
