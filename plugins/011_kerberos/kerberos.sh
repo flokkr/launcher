ae4a4b1946b93fe5a1598ac92cfffbb2a4ef7acd
Márton Elek <elek@apache.org>
2018-04-27 17:03:12 +0200

    fix kerberos dns naming usage (fqdn + krb5 sever name)

--- plugins/011_kerberos/kerberos.sh
+++ plugins/011_kerberos/kerberos.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 KERBEROS_SERVER=${KERBEROS_SERVER:-krb5}
 ISSUER_SERVER=${ISSUER_SERVER:-$KERBEROS_SERVER\:8081}
-export HOST_NAME=`hostname`
+export HOST_NAME=`hostname -f`
 for NAME in ${KERBEROS_KEYTABS}; do
    echo "Download $NAME/$HOSTNAME@... keytab file"
    wget http://$ISSUER_SERVER/keytab/$HOST_NAME/$NAME -O $CONF_DIR/$NAME.keytab
@@ -23,7 +23,7 @@ if [ -n "$KEYSTORE_DOWNLOADED" ]; then
 fi
 
 if [ -n "$KERBEROS_ENABLED" ]; then
-   sudo cp $DIR/krb5.conf /etc/krb5.conf
+   cat $DIR/krb5.conf |  sed "s/SERVER/$KERBEROS_SERVER/g" | sudo tee /etc/krb5.conf
 fi
 
 call-next-plugin "$@"
