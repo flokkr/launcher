9de0ada4decb7c142a4cd72ee78c2d91f88469ba
Márton Elek <elek@apache.org>
2018-07-28 00:24:53 +0200

    kubernetes launcher

---
+++ plugins/710_kubernetes/kubernetes.sh
@@ -0,0 +1,11 @@
+#/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+if [ "$KUBERNETES_CONFIGMAP" ]; then
+  plugin-is-active "KUBERNETES"
+  echo "Launch with kubernetes launcher. $KUBERNETES_CONFIGMAP_NAMESPACE/$KUBERNETES_CONFIGMAP"
+  $DIR/kubernetes-launcher --destination $CONF_DIR --namespace ${KUBERNETES_CONFIGMAP_NAMESPACE:-default} --configmap $KUBERNETES_CONFIGMAP $RUNTIME_ARGUMENTS
+else
+   call-next-plugin "$@"
+fi
