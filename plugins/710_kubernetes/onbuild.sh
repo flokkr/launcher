ef45e4ca6a22d1704f988796005f1deff3ee9d9a
Márton Elek <elek@apache.org>
2018-07-28 22:40:02 +0200

    instead of onbuild scripts commit the real binary scripts (easy to update)

--- plugins/710_kubernetes/onbuild.sh
+++
@@ -1,4 +0,0 @@
-#/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-wget http://kv.anzix.net//kubernetes-launcher_linux_amd64 -O $DIR/kubernetes-launcher
-chmod +x $DIR/kubernetes-launcher
