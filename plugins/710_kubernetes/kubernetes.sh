58484554ccc0f68607219d2177fc6811abf0eb52
Márton Elek <elek@apache.org>
2018-08-23 10:05:24 +0200

    use label selector instead of configmap

--- plugins/710_kubernetes/kubernetes.sh
+++ plugins/710_kubernetes/kubernetes.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ "$KUBERNETES_CONFIGMAP" ]; then
   plugin-is-active "KUBERNETES"
   echo "Launch with kubernetes launcher. $KUBERNETES_CONFIGMAP_NAMESPACE/$KUBERNETES_CONFIGMAP"
-  $DIR/kubernetes-launcher --destination $CONF_DIR --namespace ${KUBERNETES_CONFIGMAP_NAMESPACE:-default} --configmap $KUBERNETES_CONFIGMAP $RUNTIME_ARGUMENTS
+  $DIR/kubernetes-launcher --destination $CONF_DIR --namespace ${KUBERNETES_CONFIGMAP_NAMESPACE:-default} --fields metadata.name=$KUBERNETES_CONFIGMAP $RUNTIME_ARGUMENTS
 else
    call-next-plugin "$@"
 fi
