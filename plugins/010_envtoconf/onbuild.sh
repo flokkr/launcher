7d14a6ec2aa9cfd8f0e44b8c0a3166320b51e4a3
Márton Elek <elek@apache.org>
2019-12-13 23:00:03 +0100

    update envtoconf version

--- plugins/010_envtoconf/onbuild.sh
+++ plugins/010_envtoconf/onbuild.sh
@@ -1,4 +1,4 @@
 #/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-wget https://github.com/elek/envtoconf/releases/download/1.2.0/linux_amd64_envtoconf -O $DIR/envtoconf
+wget https://github.com/elek/envtoconf/releases/download/1.2.2/envtoconf_1.2.2_linux_amd64 -O $DIR/envtoconf
 chmod +x $DIR/envtoconf
