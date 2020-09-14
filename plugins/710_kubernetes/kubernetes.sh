e576f715c2e2e34a2c562e17e56fbd4a64d99cee
Elek Márton <elek@apache.org>
2020-09-14 17:53:19 +0200

    remove destination flag

--- plugins/710_kubernetes/kubernetes.sh
+++ plugins/710_kubernetes/kubernetes.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ "$KUBERNETES_CONFIGMAP" ]; then
   plugin-is-active "KUBERNETES"
   echo "Launch with kubernetes launcher."
-  $DIR/kubernetes-launcher --destination $RUNTIME_ARGUMENTS
+  $DIR/kubernetes-launcher $RUNTIME_ARGUMENTS
 else
    call-next-plugin "$@"
 fi
