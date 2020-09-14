0737e4e5210abe4ff89d30f0c207c1ba7da5e9f9
Elek Márton <elek@apache.org>
2020-09-14 17:07:43 +0200

    update kubernetes launcher

--- plugins/710_kubernetes/kubernetes.sh
+++ plugins/710_kubernetes/kubernetes.sh
@@ -4,8 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ "$KUBERNETES_CONFIGMAP" ]; then
   plugin-is-active "KUBERNETES"
-  echo "Launch with kubernetes launcher. $KUBERNETES_CONFIGMAP_NAMESPACE/$KUBERNETES_CONFIGMAP"
-  $DIR/kubernetes-launcher --destination $CONF_DIR --namespace ${KUBERNETES_CONFIGMAP_NAMESPACE:-default} --fields metadata.name=$KUBERNETES_CONFIGMAP $RUNTIME_ARGUMENTS
+  echo "Launch with kubernetes launcher."
+  $DIR/kubernetes-launcher --destination $RUNTIME_ARGUMENTS
 else
    call-next-plugin "$@"
 fi
